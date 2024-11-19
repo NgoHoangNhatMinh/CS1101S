7570249e548ddbdf431e83c05b7ba382ea590277
NgoHoangNhatMinh <ngohoangnhatminh@gmail.com>
2024-11-19 23:35:05 +0800

    Update final.js

--- final/final.js
+++ final/final.js
@@ -22,3 +22,59 @@ in the main environment, but the metamatriculator would throw
 "line ...: unbound name "..."" error
 */
 
+// 2018-2019
+
+// const :=
+
+// 2019-2020
+
+/* Question 8:
+permutations/combinations may have 2 different base case: when input is 0, and when input < 0 --> which is not a valid input. 
+*/
+    
+if (input === 0) {
+  return list(null); // as input 0 is still a valid combination/permutation
+else if (input < 0) {
+  return null; // invalid input
+}
+
+// 2D memoization:
+
+function 2d_memoize(fun) {
+  const mem = [];
+
+  function read(x,y) {
+    if (mem[x] === undefined) {
+      return undefined;
+    } else {
+      return mem[x][y];
+    }
+  }
+
+  function write(x,y,val) {
+    if (mem[x] === undefined) {
+      mem[x] = [];
+    }
+    mem[x][y] = val;
+  }
+
+  function mf(x,y) {
+    const mem_val = read(x,y);
+    if (mem_val !== undefined) {
+      return mem_val;
+    } else {
+      const result = f(x,y);
+      write(x,y,result);
+      return result;
+    }
+  }
+
+  return mf;
+}
+
+// However, the passed in function is also slightly modified
+
+/* Q9: metacirculator
+  in apply(fun, args) --> names = list("a","b",...)
+                      --> values = list(1,2,...)
+*/
